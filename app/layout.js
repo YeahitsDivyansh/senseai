d61d3282938a6371d3b349278154a726f6b339db
YeahitsDivyansh <divyansh972003@gmail.com>
2025-02-02 11:27:37 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +20,25 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+         <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          {/* footer */}
+          <footer className="bg-muted/50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-200">
+              <p>Made By Divyansh🙌</p>
+            </div>
+          </footer>
+          </ThemeProvider>
       </body>
     </html>
   );
