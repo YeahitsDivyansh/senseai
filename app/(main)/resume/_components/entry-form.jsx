2f6d7e54878ee3bae7042319f6491d7db2f0907f
YeahitsDivyansh <divyansh972003@gmail.com>
2025-02-16 00:20:48 +0530

    resume page creation initiated

---
+++ app/(main)/resume/_components/entry-form.jsx
@@ -0,0 +1,31 @@
+import { entrySchema } from "@/app/lib/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const EntryForm = ({ type, entries, onChange }) => {
+  const {
+    register,
+    handleSubmit: handleValidation,
+    formState: { errors },
+    reset,
+    watch,
+    setValue,
+  } = useForm({
+    resolver: zodResolver(entrySchema),
+    defaultValues: {
+      title: "",
+      organization: "",
+      startDate: "",
+      endDate: "",
+      description: "",
+      current: false,
+    },
+  });
+
+  const current = watch("current");
+
+  return <div>EntryForm</div>;
+};
+
+export default EntryForm;
