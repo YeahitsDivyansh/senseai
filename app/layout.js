811ae67093e640d7e7c2a4edc758f69b919e8277
YeahitsDivyansh <divyansh972003@gmail.com>
2025-12-03 14:33:41 +0530

    chore: add dotenv as a devDependency and clean up footer in layout.js

--- app/layout.js
+++ app/layout.js
@@ -32,11 +32,7 @@ export default function RootLayout({ children }) {
             <main className="min-h-screen">{children}</main>
             <Toaster richColors />
             {/* footer */}
-            <footer className="bg-muted/50 py-12">
-              <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made By Divyansh🙌</p>
-              </div>
-            </footer>
+            <footer className="bg-muted/50 py-12"></footer>
           </ThemeProvider>
         </body>
       </html>
